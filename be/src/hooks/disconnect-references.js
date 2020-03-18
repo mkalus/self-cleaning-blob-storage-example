9a48eafdd5afa9e9ee5fcc4f7e491967b3aebc19
Maximilian Kalus <info@auxnet.de>
2020-03-18 23:56:07 +0100

    More elegant solution disconnecting references.

--- be/src/hooks/disconnect-references.js
+++ be/src/hooks/disconnect-references.js
@@ -12,29 +12,40 @@ module.exports = () => {
       // get Mongoose model
       const FileReference = context.app.service('file-reference').Model;
 
-      const refs = await FileReference.find({ references: context.result._id });
+      const refs = await FileReference.find({ references: context.result._id }, {_id: true});
+      const connectedBlobs = [];
 
       if (refs) {
         for (let i = 0; i < refs.length; i++) {
-          const ref = refs[i];
+          console.log('Disconnecting ' + context.result._id + ' from blob ' + refs[i]._id);
+          connectedBlobs.push(refs[i]._id);
+        }
 
-          const pos = ref.references.indexOf(context.result._id);
-          if (pos < 0) continue;
+        // delete references from these:
+        await FileReference.updateMany(
+          { _id: { $in: connectedBlobs } },
+          { $pull: { references: context.result._id } }
+        );
 
-          // remove _id from list
-          ref.references.splice(pos, 1);
-          if (ref.references.length === 0) {
-            // delete via service
-            await context.app.service('uploads').remove(ref._id);
+        // finally, delete all orphan references within the set of the ones updated above
+        const orphans = await FileReference.find({
+          _id: { $in: connectedBlobs },
+          $or: [
+            { references: { $size: 0 } },
+            { references: { $exists: false } }
+          ]
+        });
+        if (orphans) {
+          for (let i = 0; i < orphans.length; i++) {
+            const ref = orphans[i];
+
+            console.log('Deleting orphan blob ' + ref._id);
 
             // orphaned file -> delete it!
+            await context.app.service('uploads').remove(ref._id);
+
+            // delete via service
             await FileReference.deleteOne({_id: ref._id});
-          } else {
-            // update data set
-            await FileReference.updateOne(
-              {_id: ref._id},
-              {$set: {references: ref.references}}
-            );
           }
         }
       }
